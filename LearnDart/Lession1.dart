1592f801692a80d2ec00e631b3cb496500975e2a
Ryan <anhoang800140@gmail.com>
2024-10-24 12:43:44 +0700

    First Commit

---
+++ LearnDart/Lession1.dart
@@ -0,0 +1,19 @@
+void main() {
+  //dataType variable_name = value;  (Naming convention: camel case)
+  String msg;
+  String text = 'Hello World';
+  String msg1 = text;
+  // print(msg); variable must be declared
+  print(msg1); //type inference
+  var msg2 = "hello";
+  print(msg2);
+
+  //in dart, just have two data type is int (64bit) and double (64bit)
+  //num is represent for int and double
+  int x = 10;
+  double gpa = 9.9;
+  num x1 = 20;
+  num x2 = 10.1;
+  //ctrl+alt+l is format
+  //ctrl+d is duplicate line code
+}
