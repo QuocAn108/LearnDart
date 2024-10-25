e24920e1fb5ce6edfc476762ce2ef02afd57806d
Ryan <anhoang800140@gmail.com>
2024-10-25 22:15:30 +0700

    List in Dart

---
+++ LearnDart/Lesson5.dart
@@ -0,0 +1,16 @@
+void  main(){
+  var numbers = [1,2,3,4,5,6,7];
+  numbers.add(100);
+  // numbers.add(2.365);
+  // numbers.add("sjadhkfhjkas"); just add int value, because list have some value int
+  var friends = ['An','Nhan'];
+  friends.add("vui");
+  friends.add('ve');
+  print(friends[0]);//will show "An" because an is first element of list
+  print(numbers.length);//count element
+  print(numbers[numbers.length-1]);
+  var dataType = <double>[]; //declare array , but do not declare element in list
+  var data = []; //type object, should not
+
+  //in dart do not have array, just have list
+}
