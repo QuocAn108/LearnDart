39b839ef277fe47b12fd5f6418936ba26fd5385d
Ryan <anhoang800140@gmail.com>
2024-10-30 16:22:57 +0700

    Const/final of dart

---
+++ LearnDart/Lesson8.dart
@@ -0,0 +1,10 @@
+void main(){
+  //const in dart
+  final x = 100; //can't use var to inference type
+  final double pi = 3.145926;
+  final String name = "Quoc An";
+
+  const int y = 200; // just create const in compile time
+  const friends = const []; //can use const , if u like
+  final number = []; //can't use final, can use const
+}
