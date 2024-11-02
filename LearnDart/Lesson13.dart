ca6209c2fd080c58a3892b7e22dec35d2cb0b17f
Ryan <anhoang800140@gmail.com>
2024-11-02 11:34:24 +0700

    Naming argument and Expression body in dart

---
+++ LearnDart/Lesson13.dart
@@ -0,0 +1,28 @@
+void main() {
+  int n = 10;
+  if (isEven(n: n)){
+    print("Even number");
+  }else print("Odd number");
+}
+
+printElement(int x) => print(x); //use expression body , don't need type return
+
+// isOddNumber({int? n}) => n % 2 != 0; //bool  don't want require => using nullable , but u must check null value
+
+isEven({ required int n}) => n % 2 == 0; //bool //naming argument
+
+isDivided(int n, int k) => n % k == 0; //bool
+
+bool isPrime(int n) {
+  if (n < 2) {
+    return false;
+  } else {
+    int count = 0;
+    for (int i = 2; i < n; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
